04ced8597002b16d652ae027b0ec195128f2e162
Robert Rozas N <robert.rozas.n@gmail.com>
2016-05-19 16:39:22 -0300

    Se añade template

---
+++ DjSite/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace DjSite
+{
+    public class BundleConfig
+    {
+        // Para obtener más información acerca de Bundling, consulte http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-1.11.3.js"));
+
+            
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/styles-3.css"));
+            
+        }
+    }
+}
